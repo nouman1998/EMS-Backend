64040b4130a44ed0d998d93390e7c61f3528b6b2
Misbah Khalil <misbah.khalil.az@gmail.com>
2020-06-06 22:33:22 +0500

    revert

---
+++ src/main/java/com/example/demo/Models/Department.java
@@ -0,0 +1,49 @@
+package com.example.demo.Models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Department {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String name;
+    String desription;
+
+    public Department() {
+    }
+
+    public Department(Long id, String name, String desription) {
+        this.id = id;
+        this.name = name;
+        this.desription = desription;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesription() {
+        return desription;
+    }
+
+    public void setDesription(String desription) {
+        this.desription = desription;
+    }
+}
+
---
+++ src/main/java/com/example/demo/Services/DashboardService.java
@@ -0,0 +1,11 @@
+package com.example.demo.Services;
+
+import com.example.demo.Commons.ApiResponse;
+import com.example.demo.Commons.Status;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DashboardService {
+
+
+}
