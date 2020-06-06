ed0e268a212a7a87652cb4b31b65337417cc87cc
Misbah Khalil <misbah.khalil.az@gmail.com>
2020-06-06 22:09:30 +0500

    gql_init

--- src/main/java/com/example/demo/Respositories/LeaveCodeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.Respositories;
-
-import com.example.demo.Models.LeaveCode;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LeaveCodeRepository extends JpaRepository<LeaveCode,Long>{
-}
